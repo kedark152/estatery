2e253dbb18715098c61819d7484663c14c558927
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-09-09 13:43:37 +0530

    created dummy data files

---
+++ src/dummyData/cities.js
@@ -0,0 +1,9 @@
+const cities = [
+  { value: "all", displayName: "All" },
+  { value: "mumbai", displayName: "Mumbai" },
+  { value: "pune", displayName: "Pune" },
+  { value: "navi-mumbai", displayName: "Navi Mumbai" },
+  { value: "nashik", displayName: "Nashik" },
+];
+
+export default cities;
